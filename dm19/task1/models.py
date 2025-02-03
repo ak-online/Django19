57b96c3c7928ffb01b8452de566c0c67c774dc6a
ak-online <classer@mail.ru>
2025-02-03 22:31:15 +0300

    screen shot added

--- dm19/task1/models.py
+++ dm19/task1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Buyer(models.Model):
     name = models.CharField(max_length=50)
-    balance = models.DecimalField(max_digits=6, decimal_places=3)
+    balance = models.DecimalField(max_digits=100, decimal_places=7)
     age = models.IntegerField()
 
     def __str__(self):
@@ -12,7 +12,7 @@ class Buyer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=6, decimal_places=3)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
